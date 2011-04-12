214feabef5f08888b161f18e12632281fcc3fc9f
Felix Bechstein <f@ub0r.de>
2011-04-12 20:36:29 +0200

    cleanup

--- src/de/ub0r/android/websms/connector/o2/CharacterTable.java
+++ src/de/ub0r/android/websms/connector/o2/CharacterTable.java
@@ -14,12 +14,11 @@ final class CharacterTable {
 	private static final Map<String, String> MAP = new HashMap<String, String>(
 			512);
 
-	/** Valid characters. */
-	private static String validCharacters = null;
-
 	static {
 		MAP.put("\r", "");
 		// MAP.put("´", "'");
+
+		// turkish
 		MAP.put("\u00F7", "%"); // ÷
 		MAP.put("\u0130", "I"); // İ
 		MAP.put("\u0131", "i"); // ı
@@ -30,22 +29,25 @@ final class CharacterTable {
 		MAP.put("\u011E", "G"); // Ğ
 		MAP.put("\u011F", "g"); // ğ
 
+		// polish
 		MAP.put("\u0104", "A"); // Ą
+		MAP.put("\u0105", "a"); // ą
 		MAP.put("\u0106", "C"); // Ć
+		MAP.put("\u0107", "c"); // ć
 		MAP.put("\u0118", "E"); // Ę
+		MAP.put("\u0119", "e"); // ę
 		MAP.put("\u0141", "L"); // Ł
+		MAP.put("\u0142", "l"); // ł
 		MAP.put("\u0143", "N"); // Ń
+		MAP.put("\u0144", "n"); // ń
 		MAP.put("\u00D3", "O"); // Ó
 		MAP.put("\u015A", "S"); // Ś
+		MAP.put("\u015B", "s"); // ś
 		MAP.put("\u0179", "Z"); // Ź
+		MAP.put("\u017A", "z"); // ź
 		MAP.put("\u017B", "Z"); // Ż
-
-		String[] arr = MAP.keySet().toArray(new String[0]);
-		StringBuffer strb = new StringBuffer(MAP.size());
-		for (String vc : arr) {
-			strb.append(vc);
-		}
-		validCharacters = strb.toString();
+		MAP.put("\u017C", "z"); // ż
+		MAP.put("\u00F3", "o"); // ó
 	}
 
 	/** Default constructor. */
@@ -62,7 +64,7 @@ final class CharacterTable {
 	 */
 	public static String encodeString(final String str) {
 		final int l = str.length();
-		StringBuffer strb = new StringBuffer(l);
+		final StringBuffer strb = new StringBuffer(l);
 		for (int i = 0; i < l; i++) {
 			String s = str.substring(i, i + 1);
 			String chr = MAP.get(s);
